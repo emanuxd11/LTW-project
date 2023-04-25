f93239799210823f3ae67642f1ed8eb003c1451c
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-04-25 17:02:27 +0100

    Finished the html development of the header and added a css file to style the tabs I had done before. Also minor fixes on the script.js of the main page

--- main_page/script.js
+++ main_page/script.js
@@ -1,14 +1,14 @@
 function openTab(event, AgentType) {
     var i, tabContent, selectedTab;
 
-    tabContent = document.getElementsByClassName("tabsection");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
+    tabSections = document.getElementsByClassName("tabsection");
+    for (section of tabSections) {
+        section.style.display = "none";
     }
 
     tabButtons = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    for (button of tabButtons) {
+        button.className = button.className.replace(" active", "");
     }
 
     document.getElementById(AgentType).style.display = "block";
