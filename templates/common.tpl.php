fed4ea92302bd9b44093dce74110f268ebfc37a6
Emanuel Maia <tavanomanu@gmail.com>
2023-04-25 18:13:54 +0100

    Estrutura

---
+++ templates/common.tpl.php
@@ -0,0 +1,63 @@
+<?php 
+  declare(strict_types = 1); 
+
+  require_once(__DIR__ . '/../utils/session.php');
+?>
+
+<?php function drawHeader(Session $session) { ?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>No Name Tickets</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="../javascript/script.js" defer></script>
+  </head>
+  <body>
+
+    <header>
+      <h1><a href="/">No Name Tickets</a></h1>
+      <?php 
+        if ($session->isLoggedIn()) drawLogoutForm($session);
+        else drawLoginForm($session);
+      ?>
+    </header>
+  
+    <section id="messages">
+      <?php foreach ($session->getMessages() as $messsage) { ?>
+        <article class="<?=$messsage['type']?>">
+          <?=$messsage['text']?>
+        </article>
+      <?php } ?>
+    </section>
+
+    <main>
+<?php } ?>
+
+<?php function drawFooter() { ?>
+    </main>
+
+    <footer>
+      No Name Tickets ltd. &copy; 2023
+    </footer>
+  </body>
+</html>
+<?php } ?>
+
+<?php function drawLoginForm() { ?>
+  <form action="../actions/action_login.php" method="post" class="login">
+    <input type="email" name="email" placeholder="email">
+    <input type="password" name="password" placeholder="password">
+    <a href="../pages/register.php">Register</a>
+    <button type="submit">Login</button>
+  </form>
+<?php } ?>
+
+<?php function drawLogoutForm(Session $session) { ?>
+  <form action="../actions/action_logout.php" method="post" class="logout">
+    <a href="../pages/profile.php"><?=$session->getName()?></a>
+    <button type="submit">Logout</button>
+  </form>
+<?php } ?>
