2c77f1b6ed674d1dc20a662bb563d6b12184d525
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-16 01:46:23 +0100

    Added function that loads the tickets from the database according to searchQuery, sortOrder and department and returns an array of objects of the class Ticket with the corresponding data. Also added an sql file to test the loading of said tickets

---
+++ database/loadTickets.php
@@ -0,0 +1,83 @@
+<?php
+
+use function PHPSTORM_META\map;
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/database/connection.db.php');
+
+    function loadTickets(bool $isMine = true) {
+        $db = getDatabaseConnection();
+        
+        $departmentQuery = "SELECT name FROM department WHERE id = ?"; //Query to get department name from id
+
+        
+        if ($_GET["departmentChoice"] == null) { //If the user didn't select a department, the default is "all"
+            $department = "all";
+        }
+        else {
+            $department = $_GET["departmentChoice"];
+        }
+        
+        if ($_GET["sortOrder"] == null) { //If the user didn't select a sort order, the default is "newest"
+            $sortOrder = null;
+        }
+        else {
+            $sortOrder = $_GET["sortOrder"];
+        }
+        
+        if ($_GET["ticketSearchBar"] == null) { //If the user didn't search for anything, the default is an empty string
+            $searchBar = "";
+        }
+        else {
+            $searchBar = $_GET["ticketSearchBar"];
+        }
+        
+        if ($department != "all") {
+            $stmt = $db->prepare("SELECT id FROM department WHERE name = '$department'");
+            $stmt->execute();
+            $department = $stmt->fetch()["id"];
+
+            $query = "SELECT * FROM ticket WHERE department = '$department'";
+        } else {
+            $query = "SELECT * FROM ticket WHERE 1=1";
+        }
+        
+        /* This should be changed later, in order to display just the current user's tickets
+        if($isMine) {
+            $query = $query . " AND client = '$_SESSION[username]'";
+        }
+        */
+
+        if($searchBar != "") {
+            $query = $query . " AND title LIKE '%$searchBar%'";
+        }
+
+        if($sortOrder == "newest") {
+            $query = $query . " ORDER BY id DESC";
+        } else if($sortOrder == "oldest") {
+            $query = $query . " ORDER BY id ASC";
+        }
+        
+
+        //Obtain the tickets from the database
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $tickets = $stmt->fetchAll();
+
+        //Prepare the query that will obtain the department name from the id
+        $stmt = $db->prepare($departmentQuery);
+
+        $ticket_array = array();
+
+        foreach($tickets as $ticket) {
+            $department_id = $ticket["department"];
+
+            $stmt->execute(array($department_id));
+            $department_name = $stmt->fetch()["name"];
+            
+            $ticket = new Ticket($ticket["id"], $ticket["title"], $ticket["description"], $department_name, null); //null is the client name, add it later
+            array_push($ticket_array, $ticket);
+        }
+
+        return $ticket_array;
+    }
+
+?>
