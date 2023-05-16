52c6ca7b8fbe532d0a6b5dd590687cd9f6f4e5ce
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-16 01:50:01 +0100

    Small fix on the populatetest.sql

--- database/populatetest.sql
+++ database/populatetest.sql
@@ -4,4 +4,4 @@ VALUES (1, 1, 'Issue with Payment', 'I made a payment but it is not reflecting i
        (3, 3, 'Product Inquiry', 'I have some questions regarding the specifications of your product. Can you provide more details?', '2023-05-12', '2023-05-14', 'image003.jpg', true, null), 
        (4, 1, 'Product Return Request', 'I would like to initiate a return for the product I purchased. Please guide me through the process.', '2023-05-13', '2023-05-15', 'image004.jpg', true, null),
        (5, 2, 'Website Bug Report', 'There is a bug on your website that prevents me from completing my purchase. Please fix it as soon as possible.', '2023-05-14', '2023-05-15', 'image005.jpg', true, null),
-       (6, 3, 'Marketing Campaign Inquiry', 'I'm interested in your current marketing campaigns. Can you provide more information on how to participate?', '2023-05-15', '2023-05-15', 'image006.jpg', true, null);
+       (6, 3, 'Marketing Campaign Inquiry', 'Im interested in your current marketing campaigns. Can you provide more information on how to participate?', '2023-05-15', '2023-05-15', 'image006.jpg', true, null);
