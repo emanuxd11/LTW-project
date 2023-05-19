64d6875386e87ee733ee9d6025fbca51687bf823
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-19 18:03:07 +0100

    Added edit button to name in profile page

--- templates/user_page.tpl.php
+++ templates/user_page.tpl.php
@@ -1,9 +1,11 @@
 <?php
     function drawProfileInfo($session) {
-        echo '<div class="profile-info">';
+        echo '<div class="profile">';
             echo '<img id="default_profile_image" src="/../images/default-user-image.png" alt="default-user-image.png" title="Your Profile Picture">';
-            echo '<h2>Name: ' . $session->getName() . '</h2>';
-            echo '<h3>Username: ' . $session->getUsername() . '</h3>';
+            echo '<div class="profile-info">';
+                echo '<h2>Name: ' . $session->getName() . '<img id="edit_name_button" src="/../images/edit_button.png" alt="edit_button.png" title="Change Name">' . '</h2>';
+                echo '<h3>Username: ' . $session->getUsername() . '</h3>';
+            echo '</div>';
         echo '</div>';
     }
 ?>
