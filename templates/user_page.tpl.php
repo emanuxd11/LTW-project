05dba465e3b1e691fbab2ed1ca46c28b44a1dc5a
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-19 17:28:20 +0100

    Added default user image. Also added name and username display in profile

--- templates/user_page.tpl.php
+++ templates/user_page.tpl.php
@@ -1,6 +1,7 @@
 <?php
     function drawProfileInfo($session) {
         echo '<div class="profile-info">';
+            echo '<img id="default_profile_image" src="/../images/default-user-image.png" alt="default-user-image.png" title="Your Profile Picture">';
             echo '<h2>Name: ' . $session->getName() . '</h2>';
             echo '<h3>Username: ' . $session->getUsername() . '</h3>';
         echo '</div>';
