e6f7df4c1cb13e20f06ca864a7a7f2557809d6a3
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-20 05:00:15 +0100

    Added css to edit buttons and function which I'll finish later

--- templates/user_page.tpl.php
+++ templates/user_page.tpl.php
@@ -3,9 +3,19 @@
         echo '<div class="profile">';
             echo '<img id="default_profile_image" src="/../images/default-user-image.png" alt="default-user-image.png" title="Your Profile Picture">';
             echo '<div class="profile-info">';
-                echo '<h2>Name: ' . $session->getName() . '<img id="edit_name_button" src="/../images/edit_button.png" alt="edit_button.png" title="Change Name">' . '</h2>';
-                echo '<h3>Username: ' . $session->getUsername() . '</h3>';
+                echo '<h2>Name: ' . $session->getName() . '<img class="edit_button" id="edit_name_button" src="/../images/edit_button.png" alt="edit_button.png" title="Change Name">' . '</h2>';
+                echo '<h2>Username: ' . $session->getUsername() . '<img class="edit_button" id="edit_username_button" src="/../images/edit_button.png" alt="edit_button.png" title="Change Name">' . '</h2>';
             echo '</div>';
         echo '</div>';
     }
+
+    /* Function to add later
+    function drawEditNameForm() {
+        echo '<form id="edit_name_form" action="../actions/user_edit_name.php" method="post">';
+            echo '<input type="text" name="first_name" placeholder="First Name" required>';
+            echo '<input type="text" name="last_name" placeholder="Last Name" required>';
+            echo '<button type="submit">Save</button>';
+        echo '</form>';
+    }
+    */
 ?>
