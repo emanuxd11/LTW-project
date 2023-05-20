542ba24f00dd00dc583d70a7aed530e416148627
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-05-20 17:33:07 +0100

    Deleted old populate database file

--- database/populatetest.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO ticket (id, department, title, description, post_date, last_updated, img_reference, status, agent_id)
-VALUES (1, 1, 'Issue with Payment', 'I made a payment but it is not reflecting in my account. Please help.', '2023-05-10', '2023-05-12', 'image001.jpg', true, 1),
-       (2, 2, 'Login Trouble', 'I am unable to log in to my account. It keeps showing an error message.', '2023-05-11', '2023-05-13', 'image002.jpg', true, 2),
-       (3, 3, 'Product Inquiry', 'I have some questions regarding the specifications of your product. Can you provide more details?', '2023-05-12', '2023-05-14', 'image003.jpg', true, null), 
-       (4, 1, 'Product Return Request', 'I would like to initiate a return for the product I purchased. Please guide me through the process.', '2023-05-13', '2023-05-15', 'image004.jpg', true, null),
-       (5, 2, 'Website Bug Report', 'There is a bug on your website that prevents me from completing my purchase. Please fix it as soon as possible.', '2023-05-14', '2023-05-15', 'image005.jpg', true, null),
-       (6, 3, 'Marketing Campaign Inquiry', 'Im interested in your current marketing campaigns. Can you provide more information on how to participate?', '2023-05-15', '2023-05-15', 'image006.jpg', true, null);
